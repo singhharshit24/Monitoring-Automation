a00b7c7e33ce717b1a69a9ad0aa32337ed09ac2b
singhharshit24 <singhharshit2409@gmail.com>
2025-03-04 11:07:33 +0530

    Added a numeric field for dynamic PV size input

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ def gke_deploy():
         return jsonify({"message": f"❌ Failed to update configuration: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=7000, debug=True)
